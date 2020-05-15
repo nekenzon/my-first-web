ed4724608ce3d205116070931b9b3cd644d27c67
QiHong Liang <qxl749@student.bham.ac.uk>
2020-05-15 21:21:03 +0100

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+#bibichong
+def bibi(name):
+    if name == 'bichong':
+        print("bichong")
+    elif name == 'bibi':
+        print("bibi")
+    else :
+        print("go")
+
+bibi('bichong')
