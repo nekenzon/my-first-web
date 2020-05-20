a9cc0cbb963e25630485244e37d2f0c330f1a10d
QiHong Liang <qxl749@student.bham.ac.uk>
2020-05-20 14:48:12 +0100

    Change the layout of post list page(css + html), change the order of post to ensure it shows the most recent post at the very first

--- blog/views.py
+++ blog/views.py
@@ -41,7 +41,7 @@ def post_register(request):
     return render(request, 'blog/post_register.html', {'form': form})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 
